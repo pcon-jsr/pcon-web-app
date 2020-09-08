9732c720012db36aeaa3f3f093c3e1df1f68c4c7
Aditya Pratap <adityapratap7344@gmail.com>
2020-09-08 17:59:48 +0530

    added verfication warning and fixed some styles

--- src/screens/CreateInterviewScreen/index.jsx
+++ src/screens/CreateInterviewScreen/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './index.module.scss';
+import { AuthContext } from '../../contexts/auth-context';
+import { navigationRoutes } from '../../navigation/routes';
+import Card from '../../components/Card';
+import CustomButton from '../../components/CustomButton';
 
 const CreateInterviewScreen = () => {
+    const auth = useContext(AuthContext);
+
+    if (!auth.user.verified) {
+        return (
+            <div className={styles['create-interview-screen']}>
+                <Card className={styles['verification-card']}>
+                    <h2>Verify Account!</h2>
+                    <h4>Only verified users can share interview experiences</h4>
+                    <CustomButton to={navigationRoutes.PROFILE}>
+                        VERIFY NOW
+                    </CustomButton>
+                </Card>
+            </div>
+        );
+    }
+
     return (
         <div className={styles['create-interview-screen']}>
 
