4486f9332c5cbc7a72e7778085cf232c8351e9be
Aditya Pratap <adityapratap7344@gmail.com>
2020-09-10 14:02:27 +0530

    added error handling

--- src/screens/InterviewDetailScreen/index.jsx
+++ src/screens/InterviewDetailScreen/index.jsx
@@ -7,10 +7,12 @@ import Grid from '../../components/Grid';
 import Card from '../../components/Card';
 import { getLocalDateFromFirebaseTimestamp } from '../../utils/dates';
 import Avatar from '../../components/Avatar';
+import ErrorModal from '../../components/ErrorModal';
 
 const InterviewDetailScreen = (props) => {
     const [interview, setInterview] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
 
     const interviewId = props.match.params.interviewId;
     useEffect(() => {
@@ -18,9 +20,8 @@ const InterviewDetailScreen = (props) => {
             try {
                 const interviewData = await getInterviewDocument(interviewId);
                 setInterview(interviewData);
-                console.log(interviewData);
             } catch (err) {
-                console.log(err);
+                setError(err.message);
             }
 
             setLoading(false);
@@ -30,63 +31,85 @@ const InterviewDetailScreen = (props) => {
 
     }, [interviewId]);
 
+    const clearErrorHandler = () => {
+        setError('');
+    }
+
     if (!loading && !interview) {
         return (
-            <div className={styles['interivew-detail-screen']}>
-                <h2>Interview Not Found!</h2>
-            </div>
+            <React.Fragment>
+                <ErrorModal
+                    error={error}
+                    onClear={clearErrorHandler}
+                />
+                <div className={styles['interivew-detail-screen']}>
+                    <h2>Interview Not Found!</h2>
+                </div>
+            </React.Fragment>
         );
     }
 
     if (loading) {
         return (
-            <div className={styles['interivew-detail-screen']} style={{ justifyContent: 'center' }}>
-                <LoadingSpinner />
-            </div>
+            <React.Fragment>
+                <ErrorModal
+                    error={error}
+                    onClear={clearErrorHandler}
+                />
+                <div className={styles['interivew-detail-screen']} style={{ justifyContent: 'center' }}>
+                    <LoadingSpinner />
+                </div>
+            </React.Fragment>
         );
     }
 
     return (
-        <div className={styles['interivew-detail-screen']}>
-            <h3>INTERVIEW EXPERIENCE</h3>
-            <Grid className={styles['grid']}>
-                <Card className={styles['company-card']}>
-                    <h2>{interview.companyName}</h2>
-                    <h4>{interview.year}</h4>
-                </Card>
-                <Card className={styles['author-card']}>
-                    <h4>Author</h4>
-                    <div className={styles['content']}>
-                        <Avatar src={interview.user.photoURL} className={styles['avatar']} />
-                        <div className={styles['details']}>
-                            <h2>{interview.user.name}</h2>
-                            <small>{getLocalDateFromFirebaseTimestamp(interview.createdAt)}</small>
-                            <span>{interview.user.email}</span>
-                            <span>{interview.user.registrationNum}</span>
-                            <span>{interview.user.branch}</span>
+        <React.Fragment>
+            <ErrorModal
+                error={error}
+                onClear={clearErrorHandler}
+            />
+            <div className={styles['interivew-detail-screen']}>
+                <h3>INTERVIEW EXPERIENCE</h3>
+                <Grid className={styles['grid']}>
+                    <Card className={styles['company-card']}>
+                        <h2>{interview.companyName}</h2>
+                        <h4>{interview.year}</h4>
+                    </Card>
+                    <Card className={styles['author-card']}>
+                        <h4>Author</h4>
+                        <div className={styles['content']}>
+                            <Avatar src={interview.user.photoURL} className={styles['avatar']} />
+                            <div className={styles['details']}>
+                                <h2>{interview.user.name}</h2>
+                                <small>{getLocalDateFromFirebaseTimestamp(interview.createdAt)}</small>
+                                <span>{interview.user.email}</span>
+                                <span>{interview.user.registrationNum}</span>
+                                <span>{interview.user.branch}</span>
+                            </div>
                         </div>
-                    </div>
-                </Card>
-                <Card className={styles['rounds-card']}>
-                    <p className={styles['question']}>
-                        How many rounds were there? Describe briefly.
+                    </Card>
+                    <Card className={styles['rounds-card']}>
+                        <p className={styles['question']}>
+                            How many rounds were there? Describe briefly.
                     </p>
-                    <p className={styles['answer']}>{interview.roundsDescription}</p>
-                </Card>
-                <Card className={styles['questions-card']}>
-                    <p className={styles['question']}>
-                        What questions were asked? Describe briefly.
+                        <p className={styles['answer']}>{interview.roundsDescription}</p>
+                    </Card>
+                    <Card className={styles['questions-card']}>
+                        <p className={styles['question']}>
+                            What questions were asked? Describe briefly.
                     </p>
-                    <p className={styles['answer']}>{interview.questionsDescription}</p>
-                </Card>
-                <Card className={styles['advice-card']}>
-                    <p className={styles['question']}>
-                        Any advice for future aspirants?
+                        <p className={styles['answer']}>{interview.questionsDescription}</p>
+                    </Card>
+                    <Card className={styles['advice-card']}>
+                        <p className={styles['question']}>
+                            Any advice for future aspirants?
                     </p>
-                    <p className={styles['answer']}>{interview.advice || `No specific advice`}</p>
-                </Card>
-            </Grid>
-        </div>
+                        <p className={styles['answer']}>{interview.advice || `No specific advice`}</p>
+                    </Card>
+                </Grid>
+            </div>
+        </React.Fragment>
     );
 }
 
