7a9b9b4583c76f2a372f903a59497c5a14957176
Aditya Pratap <adityapratap7344@gmail.com>
2020-09-15 18:59:08 +0530

    added alumni search feature, improved ranking algorithm

--- src/screens/AlumniScreen/index.jsx
+++ src/screens/AlumniScreen/index.jsx
@@ -5,40 +5,61 @@ import Grid from '../../components/Grid';
 import AlumniCard from '../../components/AlumniCard';
 import { alumniRef } from '../../firebase/firebase.utils';
 import LoadingSpinner from '../../components/LoadingSpinner';
+import CustomInput from '../../components/CustomInput';
+import { useForm } from '../../hooks/form-hook';
+import Card from '../../components/Card';
 
-const isFromTechGiant = (alumn) => {
-    return [
-        'google',
-        'microsoft',
-        'amazon',
-        'goldman sachs',
-        'ge healthcare',
-        'ge',
-        'shopee',
-        `lowe's india`,
-    ].includes(alumn.company.toLowerCase());
+const companyRankMap = {
+    'google': 1,
+    'microsoft': 2,
+    'amazon': 3,
+    'goldman sachs': 4,
+    'ge': 5,
+    'ge healthcare': 6,
+    'shopee': 7,
+    "lowe's india": 8,
+}
+
+const compareByCompany = (alumn1, alumn2) => {
+    const company1 = alumn1.company.toLowerCase();
+    const company2 = alumn2.company.toLowerCase();
+    if (companyRankMap[company1] && companyRankMap[company2]) {
+        if (companyRankMap[company1] === companyRankMap[company2]) {
+            return (alumn1.name < alumn2.name ? -1 : 1);
+        }
+        return companyRankMap[company1] - companyRankMap[company2];
+    } else if (companyRankMap[company1]) {
+        return -1;
+    } else if (companyRankMap[company2]) {
+        return 1;
+    } else {
+        return (alumn1.name < alumn2.name ? -1 : 1);
+    }
 }
 
 const compareAlumn = (alumn1, alumn2) => {
 
     if (alumn1.batch === alumn2.batch) {
-        if (isFromTechGiant(alumn1) && isFromTechGiant(alumn2)) {
-            return (alumn1.name < alumn2.name ? -1 : 1);
-        } else if (isFromTechGiant(alumn1) && !isFromTechGiant(alumn2)) {
-            return -1;
-        } else if (!isFromTechGiant(alumn1) && isFromTechGiant(alumn2)) {
-            return 1;
-        } else {
-            return (alumn1.name < alumn2.name ? -1 : 1);
-        }
+        return compareByCompany(alumn1, alumn2);
     }
 
     return (alumn1.batch - alumn2.batch);
 }
 
+const INITIAL_SEARCH_STATE = {
+    inputs: {
+        search: {
+            value: "",
+            isValid: true,
+        },
+    },
+    isValid: true,
+}
+
 const AlumniScreen = () => {
     const [alumniList, setAlumniList] = useState([]);
     const [loading, setLoading] = useState(true);
+    const { formState, inputHandler } = useForm(INITIAL_SEARCH_STATE);
 
     useEffect(() => {
         let listener = null;
@@ -72,7 +93,14 @@ const AlumniScreen = () => {
 
     }, []);
 
-    const renderedCards = alumniList.map(alumni => {
+    const renderedCards = alumniList.filter(alumn => {
+        const searchValue = formState.inputs.search.value.toLowerCase();
+        return (
+            alumn.name.toLowerCase().includes(searchValue)
+            || alumn.company.toLowerCase().includes(searchValue)
+            || alumn.batch.toString().toLowerCase().includes(searchValue)
+        );
+    }).map(alumni => {
         return (
             <AlumniCard key={alumni.id} alumni={alumni} />
         );
@@ -83,6 +111,21 @@ const AlumniScreen = () => {
             <ScreenTitle>ALUMNI</ScreenTitle>
             {loading && (
                 <LoadingSpinner asOverlay />
+            )}
+            {!loading && (
+                <Card className={styles['search-card']}>
+                    <form className={styles['search-form']}>
+                        <CustomInput
+                            id="search"
+                            type="text"
+                            label="Search"
+                            validators={[]}
+                            getInput={inputHandler}
+                            placeholder={`search by name or company`}
+                            initialValidity={true}
+                        />
+                    </form>
+                </Card>
             )}
             {!loading && (
                 <Grid className={styles['grid']}>
