46a32061be9a1b6324d3093f29b7dbcce3c115e3
Aditya Pratap <adityapratap7344@gmail.com>
2020-09-22 19:06:25 +0530

    testing auto update pwa feature 12

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -14,6 +14,7 @@ const HomeScreen = () => {
                         <PconLogo className={styles['logo']} />
                     </div>
                     <div className={styles['text-container']}>
+                        <h2>ONLY FOR TESTING!</h2>
                         <h2>Programming Club of <span>NIT Jamshedpur</span></h2>
                         <ReactTypingEffect
                             className={styles['animated-text']}
