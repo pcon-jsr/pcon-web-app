0f3b4236da4f3e3aea77a8abd31ddfde8ba33855
Aditya Pratap <adityapratap7344@gmail.com>
2020-09-30 19:30:09 +0530

    added new animated icon and changed theme color in manifest

--- src/screens/SplashScreen/index.jsx
+++ src/screens/SplashScreen/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './index.module.scss';
-import { ReactComponent as PconLogo } from '../../assets/pcon_logo.svg';
+// import { ReactComponent as PconLogo } from '../../assets/pcon_logo.svg';
+import PconGIF from '../../assets/pcon_logo1.gif';
 import Card from '../../components/Card';
 
 const SplashScreen = () => {
     return (
         <div className={styles['splash-screen']}>
             <Card className={styles['card']}>
-                <PconLogo className={styles['logo']} />
+                {/* <PconLogo className={styles['logo']} /> */}
+                <img alt="loading..." src={PconGIF} className={styles['logo']} />
             </Card>
         </div>
     );
