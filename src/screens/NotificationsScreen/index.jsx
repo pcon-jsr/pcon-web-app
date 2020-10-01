7447d920931af13679359ebe00311ecb754339be
Aditya Pratap <adityapratap7344@gmail.com>
2020-10-01 16:30:08 +0530

    sorting notifications

--- src/screens/NotificationsScreen/index.jsx
+++ src/screens/NotificationsScreen/index.jsx
@@ -26,6 +26,19 @@ const NotificationsScreen = () => {
                     });
                 });
 
+                notificationsData.sort((not1, not2) => {
+                    const date1 = new Date(not1.createdAt);
+                    const date2 = new Date(not2.createdAt);
+
+                    if (date1 < date2) {
+                        return 1;
+                    } else if (date1 > date2) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                });
+
                 setNotifications(notificationsData);
                 setLoading(false);
             });
